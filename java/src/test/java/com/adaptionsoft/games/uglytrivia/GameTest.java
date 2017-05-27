564001e878f518509d5cd2ec3eefd379c3367925
Xing Zhou <xing@ohdyno.me>
2017-05-27 13:42:45 -0400

    Refactor tests to use Game initialized in setup method.
    Improve readability and reuse.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -3,13 +3,20 @@ package com.adaptionsoft.games.uglytrivia;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 
+  private Game game;
+
+  @Before
+  public void setUp() throws Exception {
+    game = new Game();
+  }
+
   @Test
   public void add_alwaysReturnTrue() {
-    Game game = new Game();
     assertTrue(game.add("foo"));
     assertTrue(game.add("foo"));
     assertTrue(game.add("bar"));
@@ -17,7 +24,6 @@ public class GameTest {
 
   @Test
   public void init_creates50PopQuestions() {
-    Game game = new Game();
     assertEquals(50, game.popQuestions.size());
     for (int i = 0; i < 50; i++) {
       assertEquals("Pop Question " + i, game.popQuestions.get(i));
@@ -26,7 +32,6 @@ public class GameTest {
 
   @Test
   public void init_creates50ScienceQuestions() {
-    Game game = new Game();
     assertEquals(50, game.scienceQuestions.size());
     for (int i = 0; i < 50; i++) {
       assertEquals("Science Question " + i, game.scienceQuestions.get(i));
@@ -35,7 +40,6 @@ public class GameTest {
 
   @Test
   public void init_creates50SportsQuestions() {
-    Game game = new Game();
     assertEquals(50, game.sportsQuestions.size());
     for (int i = 0; i < 50; i++) {
       assertEquals("Sports Question " + i, game.sportsQuestions.get(i));
@@ -45,7 +49,6 @@ public class GameTest {
 
   @Test
   public void init_creates50RockQuestions() {
-    Game game = new Game();
     assertEquals(50, game.rockQuestions.size());
     for (int i = 0; i < 50; i++) {
       assertEquals("Rock Question " + i, game.rockQuestions.get(i));
